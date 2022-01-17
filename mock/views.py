aed895fa768d5575065946b45771133fa601deec
abee62 <62689173+abee62@users.noreply.github.com>
2022-01-17 11:49:29 +0530

    folder name changed back, comment added

--- mock/views.py
+++ mock/views.py
@@ -9,11 +9,11 @@ def index(request):
 #function to encrypt the message
 def encrypt(request):
     plaintext = request.GET.get('text', 'default')
-    cyphertext = '' #the cyphertext to be returned, inintitally assigned to an empty string
+    cyphertext = '' #the cyphertext to be returned, inititally assigned to an empty string
     if(len(plaintext)): 
         for plaintext_char in plaintext:#itertating over all characters in the plaintext passed
             if plaintext_char.isalpha(): 
-                n = ord('z') + ord('a') #if the character is an alphabtet, find the assci sum
+                n = ord('z') + ord('a') #if the character is an alphabtet, find the ascii sum
                 if plaintext_char.isupper():
                     n = ord('Z') + ord('A') #if character is uppercase, then modify the sum
                 new_char_pos = n - ord(plaintext_char) #find the new character
@@ -21,20 +21,22 @@ def encrypt(request):
             else:
                 cyphertext+=plaintext_char #if character is not an alphabet, append it without any change
         params ={'encrypted_text': cyphertext}
-    return render(request,'index.html', params)
+    return render(request,'index.html', params) #The encrypted text is returned
 
+
+#function to decrypt the message
 def decrypt(request):
     cyphertext = request.GET.get('text', 'default')
-    plaintext = ''
+    plaintext = '' #the plaintext to be returned, inititally assigned to an empty string
     if(len(cyphertext)):
-        for cyphertext_char in cyphertext:
+        for cyphertext_char in cyphertext: #itertating over all characters in the cyphertext passed
             if cyphertext_char.isalpha():
-                n = ord('z') + ord('a')
+                n = ord('z') + ord('a') #if the character is an alphabtet, find the ascii sum
                 if cyphertext_char.isupper():
-                    n = ord('Z') + ord('A')
-                new_char_pos = n - ord(cyphertext_char)
+                    n = ord('Z') + ord('A') #if character is uppercase, then modify the sum
+                new_char_pos = n - ord(cyphertext_char) #find the new character
                 plaintext+=chr(new_char_pos)
             else:
-                plaintext+=cyphertext_char
+                plaintext+=cyphertext_char #if character is not an alphabet, append it without any change
         params ={'decrypted_text': plaintext}
-    return render(request,'index.html', params)
+    return render(request,'index.html', params) #The decrypted text is returned
